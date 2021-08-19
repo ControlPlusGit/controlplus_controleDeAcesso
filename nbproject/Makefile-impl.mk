768923937793350ddf4b51e567e91b5ddc99cf34
Puliesi <marcos@serros.com.br>
2021-08-19 20:11:36 -0300

    adicionei a pasta nbprojetc e generated files

--- nbproject/Makefile-impl.mk
+++ nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=controlplus_controleDeAcesso
+PROJECTNAME=controlplus_controleDeAcesso-main
 
 # Active Configuration
 DEFAULTCONF=default
